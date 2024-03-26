46bf55731ed40f8a2ee5b43ce71da55f695bf019
Kate Holdener <kate.holdener@slu.edu>
2024-03-26 10:36:07 -0500

    initial code with terminal as observer

---
+++ app/src/main/java/tic_tac_toe/TicTacToe.java
@@ -0,0 +1,43 @@
+package tic_tac_toe;
+import tic_tac_toe.model.*;
+import tic_tac_toe.view.*;
+
+public class TicTacToe
+{
+   public static void main(String []args)
+   {
+      TicTacToeBoard board = new TicTacToeBoard();
+      TicTacToeTerminal terminal = new TicTacToeTerminal(board);
+
+      TicTacToePiece []playerTicTacToePieces = {TicTacToePiece.X,TicTacToePiece.O};
+
+      int nextPlayer = 0;
+      TicTacToePiece winner = null;
+      AutoPlayer autoPlayer = new AutoPlayer(TicTacToePiece.X);
+
+      while(!board.isFull())
+      {
+         autoPlayer.makeNextMove(board);
+
+         winner = board.getWinner();   
+         if (board.isFull() || winner != null)
+         {
+            break;
+         }
+         System.out.println("Next Player");
+         terminal.manualMove(playerTicTacToePieces[1]);
+
+         winner = board.getWinner();   
+         if (board.isFull() || winner != null)
+         {
+            break;
+         }
+         System.out.println("Next Player");
+      }
+
+      if (winner != null)
+      {
+         System.out.println("Winner is " + winner);
+      }
+   }
+}
---
+++ app/src/main/java/tic_tac_toe/model/Observer.java
@@ -0,0 +1,5 @@
+package tic_tac_toe.model;
+
+public interface Observer{
+   public void update();
+}
